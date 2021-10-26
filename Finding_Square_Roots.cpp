63db661869aecf51e5bc4fe9a08a9c1f457cc4d2
Ammar <ammarkhan575@gmail.com>
2021-10-26 18:50:29 +0530

    finding square using built in function

---
+++ Finding_Square_Roots.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        cout<<(int)sqrt(n)<<endl;
+    }
+return 0;
+}
