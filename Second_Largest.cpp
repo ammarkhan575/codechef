e351cb1716bbb155ebf2d2f073a22b379a4f2702
Ammar <ammarkhan575@gmail.com>
2021-10-27 01:18:28 +0530

    second largest element using sort function and printed second element of array because it is of size three and 2 element will be second largest and second smallest both

---
+++ Second_Largest.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a[3];
+        for(int i=0;i<3;i++)
+        cin>>a[i];
+        sort(a,a+n);
+        cout<<a[1]<<endl;
+    }
+return 0;
+}
