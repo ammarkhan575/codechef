f30c9393f6439947d03b1c321b5db582cb9c8b40
Ammar <ammarkhan575@gmail.com>
2021-10-26 15:59:44 +0530

    turbo sort using inbuild sort function

---
+++ Turbo_Sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,i,j;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	}
+	
+	sort(a,a+n);
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+	return 0;
+}
