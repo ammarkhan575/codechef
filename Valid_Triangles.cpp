c6233d2a77753e25fdc8df904bdc12879d55f4f2
Ammar <ammarkhan575@gmail.com>
2021-10-27 18:05:43 +0530

    valid triangle

---
+++ Valid_Triangles.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c==180)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+return 0;
+}
